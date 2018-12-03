0575dc70ae42eb856995556b96cd8100ea3a57b6
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-03 13:40:36 -0600

    finished limit-exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,3 +1,15 @@
 # OFFSET = LIMIT * PAGE - LIMIT
 
 USE employees;
+
+SELECT DISTINCT last_name FROM employees
+ORDER BY  last_name DESC
+LIMIT 10;
+
+
+
+
+SELECT first_name, last_name FROM employees
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%-12-25'
+LIMIT 5 OFFSET 45;
