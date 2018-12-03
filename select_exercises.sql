280647de49888faf78f862db45c9f815af084f7d
MattBrozusky <matthew.brozusky@gmail.com>
2018-12-03 11:44:09 -0600

    finihsed where_exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 
